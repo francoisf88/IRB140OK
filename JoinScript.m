ef74445c38308f8081709cd04f85ce4451fd74c8
lauraestaire <77013608+lauraestaire@users.noreply.github.com>
2021-05-12 18:43:26 +0200

    Add files via upload
    Just more comments but the same structure, I have doubts with the model specific parameters

---
+++ JoinScript.m
@@ -0,0 +1,106 @@
+%% Defining the robot structure
+
+%%% load parameters, from the robot datasheet
+load('data_robot_DH.mat'); %%% DH parameters  (d,a)
+load('data_robot_Dy.mat'); %%% Dynamic paramenters (m,I,r)
+
+%%% LINK ROBOT.
+L_abb(1) = Link('revolute', 'd', d1, 'a', a1, 'alpha', -pi/2,'offset',0,'m',m1,'I',I1,'r',r1); %% \theta_1
+L_abb(2) = Link('revolute', 'd', 0, 'a',a2, 'alpha', 0,'offset',-pi/2,'m',m2,'I',I2,'r',r2); %% \theta_2
+L_abb(3) = Link('revolute', 'd', 0, 'a',0, 'alpha', -pi/2,'offset',0,'m',m3,'I',I3,'r',r3); %% \theta_3
+L_abb(4) = Link('revolute', 'd', d4, 'a',0, 'alpha', pi/2,'offset',0,'m',m4,'I',I4,'r',r4); %% \theta_4
+L_abb(5) = Link('revolute', 'd', 0, 'a',0, 'alpha', -pi/2,'offset',0,'m',m5,'I',I5,'r',r5); %% \theta_5
+L_abb(6) = Link('revolute', 'd', d6, 'a',0, 'alpha', 0,'offset',-pi/2,'m',m6,'I',I6,'r',r6); %% \theta_6
+
+%----> it is needed to change the files data_robot_DH and data_robot_Dy according to the datasheet
+
+%%% joint limit %%%% (MANDATORY FOR PRISMATIC JOINT)
+L_abb(1).qlim = [-180 180]*pi/180;
+L_abb(2).qlim = [-100 100]*pi/180;
+L_abb(3).qlim = [-140 140]*pi/180;
+L_abb(4).qlim = [-200 200]*pi/180;
+L_abb(5).qlim = [-115 115]*pi/180;
+L_abb(6).qlim = [-400 400]*pi/180;
+
+%%% Create a SerialLink robot object
+MyRobot = SerialLink(L_abb,'name','abb');
+
+%%%%% Define robot position and velocity. This is the initial configuration
+q = [0 0 0 0 0 0];
+dot_q = [0 0 0 0 0 0];
+
+%%% display graphical representation of robot: This fucntion need the
+%%% configuration of my robot
+MyRobot.plot(q);
+
+%%% drive the graphical robot
+MyRobot.teach
+
+%%  Direct kinematics. Preliminar Analysis
+
+T = MyRobot.fkine(q);  
+
+%%% ----> to do
+%T_analytical = T_Analytical() %% with the simbolic toolbox you obtain the analytical version of the T 
+
+pos = T.t;
+x = pos(1);
+y = pos(2);
+z = pos(3);
+
+% To convert the homogenous transform to rotational matrix (gives me the orientation of the end effector) and translation (the position) 
+[R,t] = tr2rt(T);
+% From the T matrix gives the orientation in Euler angles
+eu = tr2eul(T);
+% The orientation matrix in Roll-Pitch-yaw
+rpy = tr2rpy(T);
+
+%%%% Geometric Jacobian (needed for the book page 154)
+J = MyRobot.jacob0(q);
+%%%% Analytixal Jacobian (eul angle rapp -ZYZ)
+Ja = MyRobot.jacob0(q,'eul'); 
+%%%% Analytixal Jacobian (rpy angle rapp)
+Ja = MyRobot.jacob0(q,'rpy'); 
+
+
+%% Robot Dynamic Model
+
+%%%%% Inertia Matrix
+B = MyRobot.inertia(q);
+%%%%% Coriolis/Centripetal Matrix
+C = MyRobot.coriolis(q,dot_q);
+%%%% Gravity Vector
+g_vect = MyRobot.gravload(q);
+
+
+%% Trajectory. Analyse the Dynamic part
+
+q0 = q; %inital position
+qf = [pi/6 0 pi/3 pi/4 0 0]; %final position
+
+Ts = 1e-1; % Samplig Time 0.1 sec
+Tf = 10; % Duration trajectory (s)
+tt = [0:Ts:10]; %% time vector
+
+[pos,vel,acc] = jtraj(q0,qf,tt);%%% trajectory both in joint space and operational space
+
+%%% Plot trajectory. Just visualization, no control
+
+MyRobot.plot(pos);
+
+% Note that the trajectory needs to be polynomial 
+figure(1)
+plot(tt,pos); grid on;xlabel('Time[s]'); ylabel('Position[rad]'); %% plot position
+legend('q_1','q_2','q_3','q_4','q_5','q_6') 
+title('Position')
+% the velocity is desirable with a bell shape
+figure(2)
+plot(tt,vel); grid on;xlabel('Time[s]'); ylabel('Velocity[rad/s]'); %% plot velocity
+legend('$\dot{q}_1$','$\dot{q}_2$','$\dot{q}_3$','$\dot{q}_4$','$\dot{q}_5$','$\dot{q}_6$','Interpreter', 'latex')
+title('Velocity')
+figure(3)
+plot(tt,acc); grid on; xlabel('Time[s]'); ylabel('Acceleration[rad/s{^2}]');%plot acceleration
+legend('$\ddot{q}_1$','$\ddot{q}_2$','$\ddot{q}_3$','$\ddot{q}_4$','$\ddot{q}_5$','$\ddot{q}_6$','Interpreter', 'latex') 
+title('Acceleration')
+
+
